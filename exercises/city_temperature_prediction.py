956d0e9f2acae1da27938958991b89d3fe127083
shaniwein <shani.weinstock@mail.huji.ac.il>
2022-04-04 20:46:11 +0300

    Q2-4 in temperature prediction.

--- exercises/city_temperature_prediction.py
+++ exercises/city_temperature_prediction.py
@@ -1,9 +1,10 @@
 import IMLearn.learners.regressors.linear_regression
-from IMLearn.learners.regressors import PolynomialFitting
+from IMLearn.learners.regressors import PolynomialFitting, polynomial_fitting
 from IMLearn.utils import split_train_test
 
 import numpy as np
 import pandas as pd
+import plotly.graph_objects as go
 import plotly.express as px
 import plotly.io as pio
 pio.templates.default = "simple_white"
@@ -42,18 +43,47 @@ if __name__ == '__main__':
     
     # Question 2 - Exploring data for specific country
     israel_data = df[df['Country'] == 'Israel']
-    px.scatter(israel_data, x=israel_data['DayOfYear'], y=israel_data['Temp'], color=israel_data['Year']).show()
+    # TODO: Are colors discrete?
+    px.scatter(
+        israel_data, x=israel_data['DayOfYear'], y=israel_data['Temp'], color=israel_data['Year'],
+        title = 'Israel Average Daily Temperature by Day of Year',
+        labels = {'DayOfYear': 'Day of Year', 'Temp': 'Temperature'}
+    )#.show()
+    
     grouped_data = israel_data[['Month', 'Temp']].groupby('Month').Temp.agg('std')
-    # TODO: Change y axis title to std
-    print(grouped_data)
-    px.bar(grouped_data, x=grouped_data.index, y=grouped_data).show()
+    px.bar(
+        grouped_data, x=grouped_data.index, y=grouped_data, 
+        title='Israel Standard Deviation of Daily Temperature by Month',
+        labels={'y': 'Temperature Standard Deviation'}
+    )#.show()
     
     # Question 3 - Exploring differences between countries
-    grouped_data = df.groupby(['Country', 'Month']).Temp.agg(['mean', 'std'])
-    print(grouped_data.index[:][1])
-    px.line(grouped_data, x=grouped_data['Month'], y=grouped_data['mean'], error_y=grouped_data['std'], color=grouped_data['Country']).show()
+    grouped_data = df.groupby(['Country', 'Month']).Temp.agg(['mean', 'std']).reset_index()
+    px.line(
+        grouped_data, x=grouped_data['Month'], y=grouped_data['mean'], error_y=grouped_data['std'], color=grouped_data['Country'],
+        title = 'Average Monthly Temperature By Country (std error bars)',
+        labels = {'mean': 'Average Temperature'}
+    )#.show()
+    
     # Question 4 - Fitting model for different values of `k`
-    raise NotImplementedError()
+    X_train, y_train, X_test, y_test = split_train_test(israel_data.drop(LABEL_COL, axis=1), israel_data[LABEL_COL], 0.75)
+    losses = {}
+    for k in range(1, 11):
+        poly_model = PolynomialFitting(k)
+        # TODO: Is this the right column?
+        poly_model.fit(X_train['DayOfYear'], y_train)
+        losses[k] = round(poly_model._loss(X_test['DayOfYear'], y_test), 2)
+    losses_df = pd.DataFrame.from_dict(losses, orient='index', columns=['loss'])
+    px.bar(losses_df, x=losses_df.index, y=losses_df['loss']).show() 
 
     # Question 5 - Evaluating fitted model on different countries
-    raise NotImplementedError()
+    chosen_k = 5
+    poly_model = PolynomialFitting(chosen_k)
+    poly_model._fit(israel_data['DayOfYear'], israel_data[LABEL_COL])
+    model_errors = {}
+    for country in df['Country'].unique():
+        if country == 'Israel':
+            continue
+        country_data = df[df['Country'] == country]
+        model_errors[country] = poly_model.loss(country_data['DayOfYear'], country_data[LABEL_COL])
+    px.Bar(x=model_errors.keys(), y=model_errors.values()).show()
