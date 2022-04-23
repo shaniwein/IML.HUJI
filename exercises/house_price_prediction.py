721107b75ffe55235e4620e25e87825c82ed3a8f
shaniwein <shani.weinstock@mail.huji.ac.il>
2022-04-22 17:19:54 -0700

    Add some stuff to house price prediction.

--- exercises/house_price_prediction.py
+++ exercises/house_price_prediction.py
@@ -16,7 +16,7 @@ DATE_FORMAT = '%Y%m%dT%H%M%S'
 NON_ZERO_COLS = ['sqft_living', 'sqft_lot', 'yr_built', 'sqft_living15', 'sqft_lot15']
 ZIPCODE_PREFIX = 'zipcode_'
 
-def parse_date(x):
+def get_timestamp(x):
     try:
         return pd.to_datetime(x, format=DATE_FORMAT).timestamp()
     except ValueError:
@@ -24,18 +24,16 @@ def parse_date(x):
 
 def preprocess_data(df):
     df = df.drop_duplicates().dropna()
-    df['timestamp'] = df['date'].apply(parse_date)
+    df['timestamp'] = df['date'].apply(get_timestamp)
     df = df.dropna()
+    df['day_of_year'] = df['date'].apply(lambda x: pd.to_datetime(x, format=DATE_FORMAT).day_of_year)
     df['max_renovated'] = df[['yr_renovated', 'yr_built']].max(axis=1)
     df = df.drop(['id', 'date', 'lat', 'long', 'yr_renovated'], axis=1)
     for col_name in df.columns:
-        limit = 1 if col_name in NON_ZERO_COLS else 0
-        df.drop(df[df[col_name] < limit].index, inplace=True)
+        df.drop(df[df[col_name] < (1 if col_name in NON_ZERO_COLS else 0)].index, inplace=True)
+    # Get rid of outstanding
+    df.drop(df[df['bedrooms'] > 30].index, inplace=True)
     df = pd.get_dummies(df, prefix=ZIPCODE_PREFIX, columns=['zipcode'], drop_first=True)
-    # TODO:
-    #   1. Add dummies for zipcode
-    #   2. Get rid of how with too many rooms
-    #   3. Change renovated to the max between renovated and built
     return df
 
 def load_data(filename: str):
@@ -76,11 +74,10 @@ def feature_evaluation(X: pd.DataFrame, y: pd.Series, output_path: str = ".") ->
         if col_name.startswith(ZIPCODE_PREFIX):
             continue
         p_corr = np.cov(col_data, y)[0][1] / (np.std(col_data) * np.std(y))
-        fig = go.Figure(
-            layout = go.Layout(
-                title = f'Pearson Correlation between {col_name} and price: {p_corr}',
-            ),
-            data = go.Scatter(x=col_data, y=y, mode='markers')
+        fig = px.scatter(
+            x=col_data, y=y,
+            title=f'Pearson Correlation between {col_name} and price: {p_corr}',
+            labels={'x': col_name, 'y': 'Price'}
         )
         fig.write_image(os.path.join(output_path, f'{col_name}_plot.png'))
 
@@ -104,21 +101,28 @@ if __name__ == '__main__':
     # Then plot average loss as function of training size with error ribbon of size (mean-2*std, mean+2*std)
     all_loss_mean = np.array([])
     all_loss_stdv = np.array([])
-    for p in np.arange(10, 101):
+    p_values = np.arange(10, 101)
+    for p in p_values:
         p_loss = np.array([])
-        for _ in range(10):
-            X_sample = X_train.sample(frac=p/100)
-            y_sample = y_train[y_train.index.isin(X_sample.index)]
+        for i in range(10):
+            # X_sample = X_train.sample(frac=p/100, replace=False)
+            # y_sample = y_train[y_train.index.isin(X_sample.index)]
+            X_sample, y_sample, _, _= split_train_test(X_train, y_train, p/100)
             lr_model = LinearRegression()
             lr_model.fit(X_sample, y_sample)
             p_loss = np.append(p_loss, lr_model.loss(X_test, y_test))
         all_loss_mean = np.append(all_loss_mean, p_loss.mean())
         all_loss_stdv = np.append(all_loss_stdv, p_loss.std())
     fig = go.Figure(
-        data = go.Scatter(x=np.arange(10, 101), y=all_loss_mean, marker_color='rgb(55, 83, 109)')
+        data = go.Scatter(x=p_values, y=all_loss_mean, marker_color='rgb(131, 90, 241)', name='loss(mean)'),
+        layout = go.Layout(
+            title = 'Mean Loss by Precentage p of Training Set',
+            xaxis = {'title': 'Precentage of Training Set'},
+            yaxis = {'title': 'Mean Loss'},
+        )
     )
     fig.add_traces([
-        go.Scatter(x=np.arange(10, 101), y=all_loss_mean+(2*all_loss_stdv), mode='lines', line_color='rgba(0,0,0,0)', fill='tonexty'),
-        go.Scatter(x=np.arange(10, 101), y=all_loss_mean-(2*all_loss_stdv), mode='lines', line_color='rgba(0,0,0,0)', showlegend=False),
+        go.Scatter(x=p_values, y=all_loss_mean+(2*all_loss_stdv), mode='lines', line_color='rgb(11, 231, 219)', fill='tonexty', name='mean(loss)+2*std(loss)'),
+        go.Scatter(x=p_values, y=all_loss_mean-(2*all_loss_stdv), mode='lines', line_color='rgba(11, 231, 219)', fill='tonexty', name='mean(loss)-2*std(loss)'),
     ])
     fig.show()
